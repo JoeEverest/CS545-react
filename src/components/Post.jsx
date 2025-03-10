71bab4b716c5c32970865cf40cd618c8d4083c0d
JoeEverest <everestjoseph11@yahoo.com>
2025-03-10 14:09:57 -0500

    optional task

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 
 function Post({ post, setSelectedPost }) {
-    return (
-        <div onClick={() => setSelectedPost(post.id)} className="border p-4 cursor-pointer mb-4 rounded-sm shadow-sm border-gray-300 hover:border-gray-400">
-            <h3 className="text-lg">{post.title}</h3>
-            <h4 className="text-sm text-gray-400">By {post.author}</h4>
-
-        </div>
-    );
+  return (
+    <div
+      onClick={() => setSelectedPost(post.id)}
+      className="border p-4 cursor-pointer mb-4 rounded-sm shadow-sm border-gray-300 hover:border-gray-400"
+    >
+      <h3 className="text-lg">{post.title}</h3>
+      <h4 className="text-sm text-gray-400">By {post.author}</h4>
+    </div>
+  );
 }
 
 export default Post;
--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -2,19 +2,16 @@ import React from "react";
 import Post from "./Post";
 
 function Posts({ posts, setSelectedPost }) {
-
-    return (
-        <div className="space-y-4">
-            <h2 className="text-xl font-semibold">Posts</h2>
-            <div className="grid grid-cols-3 gap-3">
-                {posts.map((post) => (
-                    <Post post={post} key={post.id} setSelectedPost={setSelectedPost} />
-                ))}
-            </div>
-
-
-        </div>
-    );
+  return (
+    <div className="space-y-4">
+      <h2 className="text-xl font-semibold">Posts</h2>
+      <div className="grid grid-cols-3 gap-3">
+        {posts.map((post) => (
+          <Post post={post} key={post.id} setSelectedPost={setSelectedPost} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Posts;
